85cb3f3a7df1393457f7c07253a6c9d481198738
Janst1000 <janst1000@gmail.com>
2022-10-26 12:01:22 +0200

    threshholding demo with class 6

--- test.py
+++ test.py
@@ -1,39 +1,34 @@
 import cv2
 import os
 from pprint import pprint
+from cv2 import threshold
 import numpy as np
 
-dataset = "./rami_marine_dataset/class_1/red"
+dataset = "./rami_marine_dataset/class_3/number_6"
 img_set = os.listdir(dataset)
 img_set.sort()
 
-lower = np.array([0, 0, 0])
-upper = np.array([65, 255, 255])
+#lower = np.array([0, 0, 0])
+#upper = np.array([65, 255, 255])
 
 for file in img_set:
     f = os.path.join(dataset, file)
     img = cv2.imread(f)
-    img = cv2.resize(img, (1280, 720))
+    img = cv2.resize(img, (720, 405))
     print(file)
-    hsv_img = cv2.cvtColor(img, cv2.COLOR_BGR2HSV)
-    threshhold_img = cv2.inRange(hsv_img, lower, upper)
+    #hsv_img = cv2.cvtColor(img, cv2.COLOR_BGR2HSV)
+    #threshhold_img = cv2.inRange(hsv_img, lower, upper)
     gray = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
+    red_image = img[:,:,2]
+    threshold_image = cv2.threshold(red_image, 75, 255, cv2.THRESH_TOZERO)[1]
+    canny_edge = cv2.Canny(threshold_image, 150, 200)
+    hconcat_img = np.hstack((threshold_image, canny_edge))
 
-    circles = cv2.HoughCircles(gray, cv2.HOUGH_GRADIENT, 1, 200, param1= 45, param2= 50, maxRadius=200)
 
-    if circles is not None:
-        print("Found circle")
-        circles = np.uint16(np.around(circles))
-        for i in circles[0, :]:
-            center = (i[0], i[1])
-            # circle center
-            cv2.circle(img, center, 1, (0, 100, 100), 3)
-            # circle outline
-            radius = i[2]
-            cv2.circle(img, center, radius, (255, 0, 255), 3)
+
 
     #cv2.imshow("image", threshhold_img)
-    cv2.imshow("original", img)
+    cv2.imshow("original", hconcat_img)
     key = cv2.waitKey(1)
     # 'q' to stop
     if key == ord('q'):
