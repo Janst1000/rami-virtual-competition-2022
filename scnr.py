2169db271b66e3c3f9773f35e4bca973169fbb54
Jan <janst1000@gmail.com>
2022-11-21 16:16:41 +0100

    image processing

--- scnr.py
+++ scnr.py
@@ -1,10 +1,43 @@
 import cv2
 import numpy as np
-def scnr(image):
-    b, g, r = cv2.split(image)
-    y, x, channels = image.shape
+class SCNR:
+    def __init__(self, image, amount=0.5):
+        self.image = image
+        self.amount = amount
+        self.x, self.y, channels = self.image.shape
 
-    for u in range(y):
-        for v in range(x):
-            max_rb = (r[u, v], b[u, v])
-            print(max_rb)
+    def average_neutral(self):
+        b, g, r = cv2.split(self.image)
+        for u in range(self.x):
+            for v in range(self.y):
+                m = 0.5 * (r[u, v] + b[u, v])
+                g[u, v] = min(g[u , v], m)
+
+        return cv2.merge((b, g, r))
+
+    def maximum_neutral(self):
+        b, g, r = cv2.split(self.image)
+        for u in range(self.x):
+            for v in range(self.y):
+                m = max(r[u, v], b[u, v])
+                g[u, v] = min(g[u , v], m)
+
+        return cv2.merge((b, g, r))
+
+    def additive_mask(self):
+        b, g, r = cv2.split(self.image)
+        for u in range(self.x):
+            for v in range(self.y):
+                m = min(1, r[u, v] + b[u, v])
+                g[u, v] = g[u, v] * (1 - self.amount) + (1-m) + m*g[u, v]
+
+        return cv2.merge((b, g, r))
+
+    def maximum_mask(self):
+        b, g, r = cv2.split(self.image)
+        for u in range(self.x):
+            for v in range(self.y):
+                m = max(r[u, v] , b[u, v])
+                g[u, v] = g[u, v] * (1 - self.amount) * (1-m) + m*g[u, v]
+
+        return cv2.merge((b, g, r))
--- test.py
+++ test.py
@@ -4,9 +4,10 @@ from pprint import pprint
 from cv2 import threshold
 import numpy as np
 import skimage.exposure
-from scnr import scnr
+from scnr import SCNR
+from matplotlib import pyplot as plt
 
-dataset = "./rami_marine_dataset/class_2/number_2"
+dataset = "./rami_marine_dataset/class_2/number_3"
 img_set = os.listdir(dataset)
 img_set.sort()
 
@@ -18,7 +19,7 @@ def remove_tint(img):
     h,s,v = cv2.split(hsv)
 
     # reverse the hue channel by 180 deg out of 360, so in python add 90 and modulo 180
-    h_new = (h + 120) % 180
+    h_new = (h + 90) % 180
 
     # combine new hue with old sat and value
     hsv_new = cv2.merge([h_new,s,v])
@@ -28,7 +29,6 @@ def remove_tint(img):
 
     # Get the average color of bgr_new
     ave_color = cv2.mean(bgr_new)[0:3]
-    print(ave_color)
 
     # create a new image with the average color
     color_img = np.full_like(img, ave_color)
@@ -40,39 +40,118 @@ def remove_tint(img):
     result = skimage.exposure.rescale_intensity(blend, in_range='image', out_range=(0,255)).astype(np.uint8)
     return result
 
-for file in img_set:
-    f = os.path.join(dataset, file)
-    img = cv2.imread(f)
-    img = cv2.resize(img, (720, 405))
-    print(file)
-    
-    scnr(img)
+def concat_images(*images):
+    # convert image to bgr if necassary
+    images = [cv2.cvtColor(img, cv2.COLOR_GRAY2BGR) if len(img.shape) == 2 else img for img in images]
+    hconcat_img = np.hstack(images)
+    return hconcat_img
 
-    # convert to hsv
-    hsv = cv2.cvtColor(img, cv2.COLOR_BGR2HSV)
-
-    # remove teal tint from image
-    ret = remove_tint(img)
-
-    # threshold grayscale image
-    gray = cv2.cvtColor(ret, cv2.COLOR_BGR2GRAY)
-    blur = cv2.GaussianBlur(gray, (5, 5), 0)
+def process_image(image, contrast=False):
+ 
+    blur = cv2.GaussianBlur(image, (5, 5), 0)
     # increase contrast
-    clahe = cv2.createCLAHE(clipLimit=2.0, tileGridSize=(8,8))
-    gray = clahe.apply(gray)
+    if contrast:
+        clahe = cv2.createCLAHE(clipLimit=2.0, tileGridSize=(8,8))
+        blur = clahe.apply(blur)
 
-    thresh = cv2.threshold(blur, 10, 255, cv2.THRESH_BINARY_INV + cv2.THRESH_OTSU)[1]
+    thresh = cv2.threshold(blur, 1, 255, cv2.THRESH_BINARY_INV + cv2.THRESH_OTSU)[1]
     # invert thresholdled image
     thresh = cv2.bitwise_not(thresh)
     
     thresh = cv2.cvtColor(thresh, cv2.COLOR_GRAY2BGR)
-    gray = cv2.cvtColor(gray, cv2.COLOR_GRAY2BGR)
-    hconcat_img = np.hstack((img, gray, thresh))
-    cv2.imshow("original", hconcat_img)
-    key = cv2.waitKey(1)
-    # 'q' to stop
-    if key == ord('q'):
-        break
-    # Print key 
-    elif key != -1:
-        print(key)
+    #gray = cv2.cvtColor(image, cv2.COLOR_GRAY2BGR)
+    return thresh
+
+def remove_green(img, percent = 0.5):
+        # convert to HSV
+    hsv = cv2.cvtColor(img, cv2.COLOR_BGR2HSV)
+    h,s,v = cv2.split(hsv)
+
+    # desaturate
+    s_desat = cv2.multiply(s, percent).astype(np.uint8)
+    hsv_new = cv2.merge([h,s_desat,v])
+    bgr_desat = cv2.cvtColor(hsv_new, cv2.COLOR_HSV2BGR)
+
+    # create 1D LUT for green
+    # (120 out of 360) = (60 out of 180)  +- 25
+    lut = np.zeros((1,256), dtype=np.uint8)
+    white = np.full((1,50), 255, dtype=np.uint8)
+    lut[0:1, 35:85] = white
+    print(lut.shape, lut.dtype)
+
+    # apply lut to hue channel as mask
+    mask = cv2.LUT(h, lut)
+    mask = mask.astype(np.float32) / 255
+    mask = cv2.merge([mask,mask,mask])
+
+    # mask bgr_desat and img
+    result = mask * bgr_desat + (1 - mask)*img
+    result = result.clip(0,255).astype(np.uint8)
+    return result
+
+def auto_canny(image, sigma = 0.35):
+    # compute the mediam of the single channel pixel intensities
+    v = np.median(image)
+
+    # apply automatic Canny edge detection using the computed median
+    lower = int(max(0, (1.0 - sigma) * v))
+    upper = int(min(255, (1.0 + sigma) *v))
+    edged = cv2.Canny(image, lower, upper)
+
+    # return edged image
+    return edged
+
+if __name__ == "__main__":
+    fourcc = cv2.VideoWriter_fourcc('F', 'M', 'P', '4')
+    out = cv2.VideoWriter('number3.avi',fourcc, 20, (1440, 405))
+    for file in img_set:
+        f = os.path.join(dataset, file)
+        img = cv2.imread(f)
+        img = cv2.resize(img, (720, 405))
+        print(file)
+        
+        
+        green_removed = remove_green(img, 25)
+        
+        ret = remove_tint(green_removed)
+
+        h, s, v = cv2.split(cv2.cvtColor(ret, cv2.COLOR_BGR2HSV))
+        # increase contrast in s
+        clahe = cv2.createCLAHE(clipLimit=2.0, tileGridSize=(8,8))
+        s = clahe.apply(s)
+        
+        # merge h back to hsv
+        hsv = cv2.merge([h,s,v])
+
+        img_copy = cv2.cvtColor(hsv, cv2.COLOR_HSV2BGR)
+
+        # get lab decomp
+        lab = cv2.cvtColor(img_copy, cv2.COLOR_BGR2LAB)
+        l, a, b = cv2.split(lab)
+
+            
+        # max between l and b
+        max_l_b = cv2.max(l, b)
+
+        
+        # doing normalization
+        normed = cv2.normalize(max_l_b, None, alpha=0, beta=255, norm_type=cv2.NORM_MINMAX, dtype=cv2.CV_8U)
+        
+        gray = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
+        hconcat_img = concat_images(img, normed)
+        cv2.imshow("original", hconcat_img)
+        
+        # save all frames as a video
+        #hconcat_img = cv2.resize(hconcat_img, (720, 405))
+        out.write(hconcat_img)
+        print(hconcat_img.shape)
+
+        key = cv2.waitKey(1)
+        # 'q' to stop
+        if key == ord('q'):
+            out.release()
+            break
+        # Print key 
+        elif key != -1:
+            print(key)
+    #out.release()
